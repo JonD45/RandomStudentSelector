ca7f451f65ac6265c2324bcba610875b7ffc17bd
JonD45 <jonathan.dohrman@student.swic.edu>
2025-11-17 20:54:29 -0800

    Add Dark mode

--- src/main.js
+++ src/main.js
@@ -2,9 +2,20 @@ import "./style.css";
 import data from "./db";
 const button = document.querySelector("button");
 const nameDisplay = document.querySelector("p");
+const darkModeRadio = document.querySelector("#dark");
+const lightModeRadio = document.querySelector("#light");
+
 button.addEventListener("click", () => {
   nameDisplay.textContent =
     data[getRandomIntInclusive(0, data.length - 1)].name;
+});
+darkModeRadio.addEventListener("change", () => {
+  document.body.classList.add("bg-gray-800", "text-white");
+  document.body.classList.remove("bg-gray-100");
+});
+lightModeRadio.addEventListener("change", () => {
+  document.body.classList.add("bg-gray-100");
+  document.body.classList.remove("bg-gray-800", "text-white");
 });
 function getRandomIntInclusive(min, max) {
   const minCeiled = Math.ceil(min);
